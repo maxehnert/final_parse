6b9329d6e67ed3a790d718cc0b8b90684a1217fa
Max Ehnert <max.ehnert@gmail.com>
2014-12-08 16:06:11 -0500

    ready to make customer side of website

--- scripts/main.js
+++ scripts/main.js
@@ -163,6 +163,7 @@
 var cuser = $cookieStore.get('currentUser');
   if(cuser){
     $scope.currentUsername = cuser.username;
+    $scope.currentUseremail = cuser.email;
   }
     // $scope.currentUsername = $cookieStore.get('currentUser').username;
         //console.log($scope.currentUsername);
